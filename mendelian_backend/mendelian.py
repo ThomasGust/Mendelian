a8dbb04dddb2256265a88e1313ee113fe538344f
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-18 20:03:06 -0800

    Update mendelian.py
    Add Organism class and breed function

--- mendelian_backend/mendelian.py
+++ mendelian_backend/mendelian.py
@@ -1,7 +1,9 @@
+import random
+
 class Gene:
 
     def __init__(self, a, g):
-
+        self.character = a
         assert type(a) == str and len(a) == 1
         self.allele = [a.lower(), a.upper()]
 
@@ -11,10 +13,43 @@ class Gene:
         self.a1 = self.allele[self.genotype[0]]
         self.a2 = self.allele[self.genotype[1]]
 
+        self.alleles = [self.a1, self.a2]
+
         self.j = f"{self.a1}{self.a2}"
 
 
 class Organism:
 
     def __init__(self, name, genes):
-        pass
+        self.name = name
+        self.genes = genes
+
+    def breed(self, mate):
+        assert type(mate) == Organism
+
+        mgenes = mate.genes
+
+        gene_pairs = list(zip(self.genes, mgenes))
+
+        new_genes = []
+        for gp in gene_pairs:
+            sa = random.choice(gp[0].genotype)
+            ma = random.choice(gp[1].genotype)
+
+            new_gene = Gene(gp[0].character, [sa, ma])
+            new_genes.append(new_gene)
+        
+        product = Organism(f"Child-{self.name}-{mate.name}", new_genes)
+
+        return product
+
+
+if __name__ == "__main__":
+    gene = Gene('a', [1, 0])
+    
+    m = Organism("bob", [gene])
+    f = Organism("sarah", [gene])
+
+    res = m.breed(f)
+
+    print(res.genes[0].genotype)
